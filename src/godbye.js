fceab08e767411e5f034627f51b4e0faaf753e38
eloilsonfontenele2@gmail.com <eloilsonfontenele2@gmail.com>
2022-04-10 21:30:16 -0300

    .

--- src/godbye.js
+++ src/godbye.js
@@ -3,11 +3,11 @@ module.exports = async (member, bot) => {
   let embed = new MessageEmbed()
   embed.setTitle('Ele tankou🥲')
   embed.setColor('GREEN')
-  embed.setDescription(`${member.username} não aguentou a pressão e pediu penico kkkj`)
+  embed.setDescription(`${member.user.username} não aguentou a pressão e pediu penico kkkj`)
   embed.setImage('https://c.tenor.com/XAPu_Y8VBnUAAAAS/kiss-sexy.gif')
 
   if (member.guild.id == '778374795107631104') {
-    bot.channels.cache.get('962848426943459388').send({
+    bot.channels.cache.get('962871783172743219').send({
       embeds: [embed]
     })
   }
